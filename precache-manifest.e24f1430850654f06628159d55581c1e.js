6a217ad886edca41bca4dcdcfeda14e18930bab6
uzairamer <m.uzair.amer@gmail.com>
2020-08-05 00:28:58 +0500

    Updates

---
+++ precache-manifest.e24f1430850654f06628159d55581c1e.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cea75eb4a41b6efe144425da3ba52519",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2aa554c8058924e81c04",
+    "url": "/portfolio/static/css/2.350fe17a.chunk.css"
+  },
+  {
+    "revision": "5d7939fcea2830a87ee8",
+    "url": "/portfolio/static/css/main.7b4f9793.chunk.css"
+  },
+  {
+    "revision": "2aa554c8058924e81c04",
+    "url": "/portfolio/static/js/2.c5cf626f.chunk.js"
+  },
+  {
+    "revision": "a73511fdd0e79fea52c5f42ad1574633",
+    "url": "/portfolio/static/js/2.c5cf626f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d7939fcea2830a87ee8",
+    "url": "/portfolio/static/js/main.9293b5c7.chunk.js"
+  },
+  {
+    "revision": "35ed240fb4f96ce71694",
+    "url": "/portfolio/static/js/runtime-main.e6d3e2f2.js"
+  },
+  {
+    "revision": "db56f3e8c7a796df546d682f6b21e44a",
+    "url": "/portfolio/static/media/aitchison_portal.db56f3e8.png"
+  },
+  {
+    "revision": "8f018d3eb25b762ea15586769258982f",
+    "url": "/portfolio/static/media/apw.8f018d3e.png"
+  },
+  {
+    "revision": "a17bc98abbb7f33d0b024cb440d87a72",
+    "url": "/portfolio/static/media/avatar5.a17bc98a.jpg"
+  },
+  {
+    "revision": "fa36b8699fe66b65157d6f094fa820a6",
+    "url": "/portfolio/static/media/coding.fa36b869.png"
+  },
+  {
+    "revision": "1fa31e533e3e4916b196e4875eef46e9",
+    "url": "/portfolio/static/media/nellak.1fa31e53.jpg"
+  },
+  {
+    "revision": "7d815f0485fb9718160e44bd76e92fed",
+    "url": "/portfolio/static/media/slappedham.7d815f04.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/portfolio/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/portfolio/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/portfolio/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/portfolio/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "acfec7a0f9fea4b8c433e815425557d5",
+    "url": "/portfolio/static/media/suficity.acfec7a0.png"
+  },
+  {
+    "revision": "9fbfcc072ca0bd833de5471c8c10fa07",
+    "url": "/portfolio/static/media/tasneem.9fbfcc07.jpg"
+  }
+]);
